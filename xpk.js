fa07cc3e230a8a4a81e6a26819c5f68c24c0f527
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-08-10 00:20:35 +0200

    Create xpk.js

---
+++ xpk.js
@@ -0,0 +1,16 @@
+const Antenna = require('iotex-antenna');
+
+(async () => {
+  const antenna = new Antenna("https://babel-api.mainnet.iotex.io");
+
+  // recover the whole wallet from a single private key
+  const unlockedWallet = antenna.iotx.accounts.privateKeyToAccount(
+    "69805ee813eadffa8fae53d0e6063e5fbf6a6e0fb9e90f6eaad7bc67f3d6c4bd"
+  );
+
+  // get the balance, nonce, number of actions, pendingNonce of the account
+  const accountDetails = await antenna.iotx.getAccount({
+    address: wallet.address
+  });
+  console.log(accountDetails);
+})();
