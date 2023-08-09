e104229aeb56f21146fef06c7cc3dcadcce8b8fb
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-08-10 00:40:05 +0200

    Update xpk.js

--- xpk.js
+++ xpk.js
@@ -4,7 +4,7 @@ const Antenna = require('iotex-antenna');
   const antenna = new Antenna("https://babel-api.mainnet.iotex.io");
 
   // recover the whole wallet from a single private key
-  const unlockedWallet = antenna.iotx.accounts.privateKeyToAccount(
+  const wallet = antenna.iotx.accounts.privateKeyToAccount(
     "69805ee813eadffa8fae53d0e6063e5fbf6a6e0fb9e90f6eaad7bc67f3d6c4bd"
   );
 
